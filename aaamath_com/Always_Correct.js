0a6b98f9eb08b69e9b5cf80d8bc66ee88a7d57f7
Bodhith <34181215+Bodhith@users.noreply.github.com>
2019-10-03 07:14:39 +0530

    Create Always_Correct.js

---
+++ aaamath_com/Always_Correct.js
@@ -0,0 +1,13 @@
+function alpha()
+{
+   if( !(numF%7) )
+   {
+    document.getElementsByTagName("input")[2].click();
+   }
+   else
+   {
+    document.getElementsByTagName("input")[3].click();
+   }
+}
+
+alpha();
